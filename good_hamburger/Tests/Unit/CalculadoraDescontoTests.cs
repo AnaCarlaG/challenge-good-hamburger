84031f27f222c533797f92bc62c9cb1d711a29ca
acgsilva98 <ana.carla@qintess.com>
2026-04-26 12:45:04 -0300

    renomear os projetos

--- good_hamburger/Tests/Unit/CalculadoraDescontoTests.cs
+++ good_hamburger/Tests/Unit/CalculadoraDescontoTests.cs
@@ -2,11 +2,6 @@
 using Domain.Enum;
 using Domain.Util;
 using FluentAssertions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tests.Unit
 {
--- good_hamburger/Tests/Unit/PedidoServiceTests.cs
+++ good_hamburger/Tests/Unit/PedidoServiceTests.cs
@@ -1,17 +1,9 @@
 ﻿using Application.Service;
-using Application.Service.Interface;
 using Domain.Entities;
 using Domain.Enum;
 using Domain.Exceptions;
 using Domain.Interfaces.Repository;
 using Moq;
-using NSubstitute;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit.Abstractions;
 
 namespace Tests.Unit
 {
